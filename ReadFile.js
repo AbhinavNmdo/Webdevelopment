58aebe899b80c70dce846037cead87f856c0dc82
Abhinav Namdeo <abhaynam22@gmail.com>
2021-08-09 19:44:58 +0530

    Initial Commit

---
+++ ReadFile.js
@@ -0,0 +1,11 @@
+// const fs = require("fs")
+// let bob = fs.readFileSync("abhay.txt", "utf8")
+
+// console.log("Its a content of the abhay.txt")
+// console.log(bob)
+
+// bob = bob.replace("abhay", "ashu")
+// console.log("Creating new file....")
+// fs.writeFileSync("ashu.txt", bob)
+
+
---
+++ Syncro&ASyncro.js
@@ -0,0 +1,50 @@
+// Synchronous or blocking
+// - line by line execution
+
+// Asynchronous or non-blocking
+// - line by line execution not guaranteed
+// - callbacks will fire
+
+const fs = require("fs")
+fs.readFile("abhay.txt", "utf-8", (a, b)=>{
+    console.log(b)
+})
+console.log("This is a message 1")
+a = 4545554545
+b = 78787878787
+console.log(a+b)
+console.log("This is a message 2")
+console.log("This is a message 3")
+console.log("This is a message 4")
+console.log("This is a message 5")
+console.log("This is a message 1")
+console.log("This is a message 2")
+console.log("This is a message 3")
+console.log("This is a message 4")
+console.log("This is a message 5")
+console.log("This is a message 1")
+console.log("This is a message 2")
+console.log("This is a message 3")
+console.log("This is a message 4")
+console.log("This is a message 5")
+console.log("This is a message 1")
+console.log("This is a message 2")
+console.log("This is a message 3")
+console.log("This is a message 4")
+console.log("This is a message 5")
+console.log("This is a message 1")
+console.log("This is a message 2")
+console.log("This is a message 3")
+console.log("This is a message 4")
+console.log("This is a message 5")
+console.log("This is a message 1")
+console.log("This is a message 2")
+console.log("This is a message 3")
+console.log("This is a message 4")
+console.log("This is a message 5")
+console.log("This is a message 1")
+console.log("This is a message 2")
+console.log("This is a message 3")
+console.log("This is a message 4")
+console.log("This is a message 5")
+
