0847b3c83b635b2be7f936e305f39d0ef9727f0b
Abhinav Namdeo <abhaynam22@gmail.com>
2021-09-21 12:02:53 +0530

    Initial Comit

--- LearnJavascript/AlarmApp/index.js
+++ LearnJavascript/AlarmApp/index.js
@@ -1,24 +1,52 @@
 console.log("Welcome to Alarm App");
 let btn = document.getElementById('submittime');
+let btn1 = document.getElementById('stopalarm');
+
+btn1.addEventListener('click', function (e) {
+    e.preventDefault();
+    stopAlarm();
+});
+
 btn.addEventListener('click', function (e) {
     e.preventDefault();
     let alarm = document.getElementById('alarmtime');
     let alarmtype = document.getElementById('alarmtype');
-    if(alarmtype.value == 'date'){
-        time1 = (alarm.value)*84600000;
-        console.log(time1);
-    }
-    else if(alarmtype.value == 'hour'){
-        time1 = (alarm.value)*3600000;
-        console.log(time1);
-    }
-    else if(alarmtype.value == 'min'){
-        time1 = (alarm.value)*60000;
-        console.log(time1);
-    }
-    else if(alarmtype.value == 'sec'){
-        time1 = (alarm.value)*1000;
-        console.log(time1);
+    if(alarm.value.length>0){
+        if(alarmtype.value == 'date'){
+            time1 = (alarm.value)*84600000;
+            console.log(time1);
+        }
+        else if(alarmtype.value == 'hour'){
+            time1 = (alarm.value)*3600000;
+            console.log(time1);
+        }
+        else if(alarmtype.value == 'min'){
+            time1 = (alarm.value)*60000;
+            console.log(time1);
+        }
+        else if(alarmtype.value == 'sec'){
+            time1 = (alarm.value)*1000;
+            console.log(time1);
+        }
     }
     alarm.value = "";
+    alarmtype.value = "null";
+    ringAlarm(time1);
 });
+
+var audio = new Audio('https://kukuklok.com/audio/alien.mp3');
+let form = document.getElementById('form');
+let bell = document.getElementsById('bell');
+function ringAlarm(timer) {
+    setTimeout(() => {
+        audio.play();
+    }, timer);
+    // form.style.display = "none";
+    bell.style.display = "block";
+}
+function stopAlarm() {
+    audio.pause();
+    audio.currentTime = 0;
+    form.style.display = "flex";
+    bell.style.display = "none";
+}
