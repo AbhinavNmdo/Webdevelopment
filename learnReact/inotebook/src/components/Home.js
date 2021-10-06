40d64a00bbed3065b1576127c37ef89a001197d1
Abhinav Namdeo <abhaynam22@gmail.com>
2021-10-06 19:08:05 +0530

    Initial Commit

--- learnReact/inotebook/src/components/Home.js
+++ learnReact/inotebook/src/components/Home.js
@@ -1,20 +1,42 @@
 import React, { useContext, useEffect } from "react";
-import NoteContext from "../context/Notes/NoteContext";
 
 const Home = () => {
-  const a = useContext(NoteContext);
-  
-  useEffect(() => {
-    return () => {
-      a.update();
-      // eslint-disable-next-line
-    }
-  }, [a])
-
   return (
-    <div>
-      <h1>I am {a.state.name} and my role is {a.state.role}</h1>
-    </div>
+    <>
+      <div>
+        <div className="container" style={{height: '90vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+        <h1 align="center" className="my-4">
+          iNoteBook
+        </h1>
+          <div className="card p-4 mb-5">
+            <form style={{width: '500px'}}>
+              <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label">
+                  <strong>Title</strong>
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  min="1"
+                />
+              </div>
+              <div className="mb-3">
+                <label for="exampleInputPassword1" className="form-label">
+                  <strong>Description</strong>
+                </label>
+                <textarea className="form-control" id="exampleInputPassword1" />
+                <div id="emailHelp" class="form-text">Scroll to see your Notes</div>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
