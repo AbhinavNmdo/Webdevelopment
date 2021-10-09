21bb5a38c635ca4261021b2b862574b5064ca991
Abhinav Namdeo <abhaynam22@gmail.com>
2021-10-09 17:32:29 +0530

    Initial Commit

--- learnReact/inotebook/src/components/NotesCards.js
+++ learnReact/inotebook/src/components/NotesCards.js
@@ -4,20 +4,32 @@ import NoteContext from "../context/Notes/NoteContext";
 const NotesCards = (props) => {
   const context = useContext(NoteContext);
   const { deleteNote } = context;
-  const { note } = props;
+  const { note, updateNote } = props;
   return (
-    <div className="col-md-3 my-3">
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
-          <p className="card-text">{note.description}</p>
-          <div>
-            <i className="fas fa-trash mx-2" onClick={()=>{deleteNote(note._id)}}></i>
-            <i className="fas fa-edit mx-2"></i>
+    <>
+      <div className="col-md-3 my-3">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{note.title}</h5>
+            <p className="card-text">{note.description}</p>
+            <div>
+              <i
+                className="fas fa-trash mx-2"
+                onClick={() => {
+                  deleteNote(note._id);
+                }}
+              ></i>
+              <i
+                className="fas fa-edit mx-2"
+                onClick={() => {
+                  updateNote(note);
+                }}
+              ></i>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- learnReact/inotebook/src/components/NotesContent.js
+++ learnReact/inotebook/src/components/NotesContent.js
@@ -1,20 +1,123 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import NoteContext from "../context/Notes/NoteContext";
 import NotesCards from "./NotesCards";
 
 const NotesContent = () => {
   const context = useContext(NoteContext);
-  const {notes, getNote} = context;
+  const ref = useRef(null);
+  const { notes, getNote } = context;
+
   useEffect(() => {
-    getNote()
+    getNote();
   }, []);
+
+  const [note, setNote] = useState({etitle: "", edescription: ""})
+
+  const updateNote = (currentNote)=>{
+    ref.current.click();
+    setNote({etitle: currentNote.title, edescription: currentNote.description})
+  }
+
+  const handleClick = (e)=>{
+    console.log("Updating notes....");
+    e.preventDefault();
+  }
+
+  const handleChange = (e)=>{
+    setNote({...note, [e.target.name]: e.target.value})
+  }
+
   return (
-    <div className="row">
-      <h1 style={{ display: "flex", justifyContent: "center" }}>Your iNotes</h1>
-      {notes.map((note)=>{
-          return <NotesCards key={note._id} note={note}/>
-      })}
-    </div>
+    <>
+      <button
+        type="button"
+        class="btn btn-primary d-none"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        ref={ref}
+      >
+        Launch
+      </button>
+
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Edit Note
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="etitle" className="form-label">
+                    <strong>Title</strong>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="etitle"
+                    name="etitle"
+                    aria-describedby="emailHelp"
+                    min="1"
+                    onChange={handleChange}
+                    value={note.etitle}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="edescription" className="form-label">
+                    <strong>Description</strong>
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="edescription"
+                    onChange={handleChange}
+                    name="edescription"
+                    value={note.edescription}
+                  />
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={handleClick}>
+                Update
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <div className="row">
+        <h1 style={{ display: "flex", justifyContent: "center" }}>
+          Your iNotes
+        </h1>
+        {notes.map((note) => {
+          return <NotesCards key={note._id} note={note} updateNote={updateNote}/>;
+        })}
+      </div>
+    </>
   );
 };
 
